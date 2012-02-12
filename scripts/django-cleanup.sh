46d92f076b36020a76bd2dd6340522ac84e54f09
Martin Brochhaus <mbrochh@gmail.com>
2012-02-12 20:43:38 +0100

    Fixed bug with django-cleanup.sh

--- scripts/django-cleanup.sh
+++ scripts/django-cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source script-settings-INSERT_PROJECTNAME.sh
+source $HOME/bin/script-settings-INSERT_PROJECTNAME.sh
 source $HOME/Envs/$VENV_NAME/bin/activate
 
 $HOME/webapps/$DJANGO_APP_NAME/project/manage.py cleanup

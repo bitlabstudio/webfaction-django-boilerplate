0b97b6da3c18ec53accd05e1558cedc681b109a6
Martin Brochhaus <mbrochh@gmail.com>
2012-03-15 19:19:30 +0800

    install_everything now takes care of adding the database information to the .pgpass file on the server.

--- fabfile.py
+++ fabfile.py
@@ -16,7 +16,7 @@ from fabric.api import (
     run,
     settings,
 )
-from fabric.contrib.files import append, exists,  sed
+from fabric.contrib.files import append, contains, exists,  sed
 
 import fabric_settings as fab_settings
 
@@ -30,6 +30,19 @@ BASHRC_SETTING2 = 'export WORKON_HOME=$HOME/Envs'
 BASHRC_SETTING3 = 'source /home/{0}/bin/virtualenvwrapper.sh'.format(env.user)
 BASHRC_SETTING4 = 'export PIP_VIRTUALENV_BASE=$WORKON_HOME'
 BASHRC_SETTING5 = 'export PIP_RESPECT_VIRTUALENV=true'
+
+PROJECT_NAME = fab_settings.PROJECT_NAME
+FILE_SCRIPT_SETTINGS = 'script-settings-{0}.sh'.format(PROJECT_NAME)
+FILE_DEPLOY_WEBSITE = 'deploy-website-{0}.sh'.format(PROJECT_NAME)
+FILE_MYSQL_BACKUP = 'mysql-backup-{0}.sh'.format(PROJECT_NAME)
+FILE_PG_BACKUP = 'pg-backup-{0}.sh'.format(PROJECT_NAME)
+FILE_LOCALE_BACKUP = 'locale-backup-{0}.sh'.format(PROJECT_NAME)
+FILE_RESTART_APACHE = 'restart-apache-{0}.sh'.format(PROJECT_NAME)
+FILE_DJANGO_CLEANUP = 'django-cleanup-{0}.sh'.format(PROJECT_NAME)
+FILE_CRONTAB = 'crontab-{0}.txt'.format(PROJECT_NAME)
+FILE_SHOW_MEMORY = 'show-memory.sh'
+FILE_PGPASS = '.pgpass-{0}'.format(PROJECT_NAME)
+
 
 
 # ****************************************************************************
@@ -47,6 +60,7 @@ def first_deployment():
     run_create_virtualenv()
     run_clone_repo()
     run_install_scripts()
+    run_install_pgpass()
     run_install_crontab()
     run_prepare_wsgi()
     run_install_requirements()
@@ -92,7 +106,7 @@ def local_create_fab_settings():
     with lcd(fabfile_dir):
         local('cp fab_settings.py.sample fab_settings.py')
         local("sed -i -r -e 's/INSERT_PROJECT_NAME/{0}/g'"
-              " fab_settings.py".format(fab_settings.PROJECT_NAME))
+              " fab_settings.py".format(PROJECT_NAME))
         local("sed -i -r -e 's/INSERT_ENV_USER/{0}/g'"
               " fab_settings.py".format(fab_settings.ENV_USER))
 
@@ -144,13 +158,13 @@ def run_add_bashrc_settings():
 
 def run_clone_repo():
     run('mkdir -p $HOME/src')
-    cloned_repo_path = '$HOME/src/{0}'.format(fab_settings.PROJECT_NAME)
+    cloned_repo_path = '$HOME/src/{0}'.format(PROJECT_NAME)
     if exists(cloned_repo_path):
         run('rm -rf {0}'.format(cloned_repo_path))
     with cd('$HOME/src'):
         run('git clone $HOME/webapps/git/repos/{0} {1}'.format(
-            fab_settings.GIT_REPO_NAME, fab_settings.PROJECT_NAME))
-    with cd('$HOME/src/{0}'.format(fab_settings.PROJECT_NAME)):
+            fab_settings.GIT_REPO_NAME, PROJECT_NAME))
+    with cd('$HOME/src/{0}'.format(PROJECT_NAME)):
         run('git submodule init')
         run('git submodule update')
 
@@ -191,8 +205,11 @@ def run_delete_previous_attempts():
     run('rm -rf $HOME/webapps/{0}/project'.format(
         fab_settings.DJANGO_APP_NAME))
     run('rm -rf $HOME/Envs/{0}/'.format(fab_settings.VENV_NAME))
-    run('rm -rf $HOME/src/{0}/'.format(fab_settings.PROJECT_NAME))
-    run('rm -rf $HOME/bin/*{0}*.*'.format(fab_settings.PROJECT_NAME))
+    run('rm -rf $HOME/src/{0}/'.format(PROJECT_NAME))
+    run('rm -rf $HOME/bin/*{0}*.*'.format(PROJECT_NAME))
+    with cd('$HOME'):
+        run("sed '/{0}/d' .pgpass > .pgpass_tmp".format(fab_settings.DB_NAME))
+        run('mv .pgpass_tmp .pgpass')
     # TODO remove crontab jobs
 
 
@@ -202,7 +219,7 @@ def run_deploy_website(with_manage_py=True):
         args = ''
 
     run('workon {0} && deploy-website-{1}.sh{2}'.format(fab_settings.VENV_NAME,
-        fab_settings.PROJECT_NAME, args))
+        PROJECT_NAME, args))
 
 
 def run_install_crontab():
@@ -210,7 +227,7 @@ def run_install_crontab():
     with cd('$HOME/bin/'):
         run('crontab -l > crontab_tmp')
         run('cat crontab-{0}.txt >> crontab_tmp'.format(
-            fab_settings.PROJECT_NAME))
+            PROJECT_NAME))
         run('crontab crontab_tmp')
         run('rm crontab_tmp')
 
@@ -220,54 +237,60 @@ def run_install_mercurial():
         run('easy_install-2.7 mercurial')
 
 
+def run_install_pgpass():
+    with cd('$HOME'):
+        run('touch .pgpass')
+        if not contains('.pgpass', fab_settings.DB_NAME):
+            run('cat {0} > .pgpass'.format(FILE_PGPASS))
+        run('rm {0}'.format(FILE_PGPASS))
+
+
 def run_install_requirements():
     run('workon {0} && pip install -r $HOME/src/{1}/website/webapps/django/'
         'project/requirements.txt --upgrade'.format(
-            fab_settings.VENV_NAME, fab_settings.PROJECT_NAME))
+            fab_settings.VENV_NAME, PROJECT_NAME))
 
 
 def run_install_scripts():
-    project_name = fab_settings.PROJECT_NAME
-    script_settings_name = 'script-settings-{0}.sh'.format(project_name)
-    deploy_website_name = 'deploy-website-{0}.sh'.format(project_name)
-    mysql_backup_name = 'mysql-backup-{0}.sh'.format(project_name)
-    pg_backup_name = 'pg-backup-{0}.sh'.format(project_name)
-    locale_backup_name = 'locale-backup-{0}.sh'.format(project_name)
-    restart_apache_name = 'restart-apache-{0}.sh'.format(project_name)
-    django_cleanup_name = 'django-cleanup-{0}.sh'.format(project_name)
-    crontab_name = 'crontab-{0}.txt'.format(project_name)
-    show_memory_name = 'show-memory.sh'
-    with cd('$HOME/src/{0}/scripts'.format(project_name)):
+    with cd('$HOME/src/{0}/scripts'.format(PROJECT_NAME)):
         run('git pull origin master')
-        run('cp deploy-website.sh $HOME/bin/{0}'.format(deploy_website_name))
-        run('cp mysql-backup.sh $HOME/bin/{0}'.format(mysql_backup_name))
-        run('cp pg-backup.sh $HOME/bin/{0}'.format(pg_backup_name))
-        run('cp locale-backup.sh $HOME/bin/{0}'.format(locale_backup_name))
-        run('cp restart-apache.sh $HOME/bin/{0}'.format(restart_apache_name))
-        run('cp django-cleanup.sh $HOME/bin/{0}'.format(django_cleanup_name))
-        run('cp script-settings.sh $HOME/bin/{0}'.format(script_settings_name))
-        run('cp crontab.txt $HOME/bin/{0}'.format(crontab_name))
-        run('cp {0} $HOME/bin/{0}'.format(show_memory_name))
+        run('cp deploy-website.sh $HOME/bin/{0}'.format(FILE_DEPLOY_WEBSITE))
+        run('cp mysql-backup.sh $HOME/bin/{0}'.format(FILE_MYSQL_BACKUP))
+        run('cp pg-backup.sh $HOME/bin/{0}'.format(FILE_PG_BACKUP))
+        run('cp locale-backup.sh $HOME/bin/{0}'.format(FILE_LOCALE_BACKUP))
+        run('cp restart-apache.sh $HOME/bin/{0}'.format(FILE_RESTART_APACHE))
+        run('cp django-cleanup.sh $HOME/bin/{0}'.format(FILE_DJANGO_CLEANUP))
+        run('cp script-settings.sh $HOME/bin/{0}'.format(FILE_SCRIPT_SETTINGS))
+        run('cp crontab.txt $HOME/bin/{0}'.format(FILE_CRONTAB))
+        run('cp {0} $HOME/bin/{0}'.format(FILE_SHOW_MEMORY))
+
+        # This one goes to $HOME
+        run('cp .pgpass $HOME/{0}'.format(FILE_PGPASS))
 
     with cd('$HOME/bin'):
-        sed(script_settings_name, 'INSERT_USERNAME', fab_settings.ENV_USER)
-        sed(script_settings_name, 'INSERT_DB_USER', fab_settings.DB_USER)
-        sed(script_settings_name, 'INSERT_DB_NAME', fab_settings.DB_NAME)
-        sed(script_settings_name, 'INSERT_DB_PASSWORD',
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_USERNAME', fab_settings.ENV_USER)
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_DB_USER', fab_settings.DB_USER)
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_DB_NAME', fab_settings.DB_NAME)
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_DB_PASSWORD',
             fab_settings.DB_PASSWORD)
-        sed(script_settings_name, 'INSERT_PROJECT_NAME', project_name)
-        sed(script_settings_name, 'INSERT_DJANGO_APP_NAME',
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_PROJECT_NAME', PROJECT_NAME)
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_DJANGO_APP_NAME',
             fab_settings.DJANGO_APP_NAME)
-        sed(script_settings_name, 'INSERT_VENV_NAME', fab_settings.VENV_NAME)
-        sed(deploy_website_name, 'INSERT_PROJECTNAME', project_name)
-        sed(mysql_backup_name, 'INSERT_PROJECTNAME', project_name)
-        sed(pg_backup_name, 'INSERT_PROJECTNAME', project_name)
-        sed(locale_backup_name, 'INSERT_PROJECTNAME', project_name)
-        sed(restart_apache_name, 'INSERT_PROJECTNAME', project_name)
-        sed(django_cleanup_name, 'INSERT_PROJECTNAME', project_name)
-        sed(crontab_name, 'INSERT_PROJECTNAME', project_name)
-        sed(show_memory_name, 'INSERT_PROJECTNAME', project_name)
+        sed(FILE_SCRIPT_SETTINGS, 'INSERT_VENV_NAME', fab_settings.VENV_NAME)
+        sed(FILE_DEPLOY_WEBSITE, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_MYSQL_BACKUP, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_PG_BACKUP, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_LOCALE_BACKUP, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_RESTART_APACHE, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_DJANGO_CLEANUP, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_CRONTAB, 'INSERT_PROJECTNAME', PROJECT_NAME)
+        sed(FILE_SHOW_MEMORY, 'INSERT_PROJECTNAME', PROJECT_NAME)
         run('rm -f *.bak')
+
+    with cd('$HOME'):
+        sed(FILE_PGPASS, 'INSERT_DB_NAME', fab_settings.DB_NAME)
+        sed(FILE_PGPASS, 'INSERT_DB_USER', fab_settings.DB_USER)
+        sed(FILE_PGPASS, 'INSERT_DB_PASSWORD', fab_settings.DB_PASSWORD)
 
 
 def run_install_virtualenv():
@@ -297,7 +320,7 @@ def run_prepare_local_settings():
         sed('local_settings.py', '"PASSWORD": ""', '"PASSWORD": "{0}"'.format(
             fab_settings.DB_PASSWORD))
         sed('local_settings.py', 'yourproject', '{0}'.format(
-            fab_settings.PROJECT_NAME))
+            PROJECT_NAME))
         sed('local_settings.py', 'FROM_EMAIL = "info@example.com"',
             'FROM_EMAIL = "{0}"'.format(fab_settings.EMAIL_DEFAULT_FROM_EMAIL))
         sed('local_settings.py', 'EMAIL_BACKEND', '#EMAIL_BACKEND')
@@ -327,7 +350,7 @@ def run_prepare_wsgi():
     with cd('$HOME/webapps/{0}'.format(fab_settings.DJANGO_APP_NAME)):
         run('rm -rf myproject')
         run('cp $HOME/src/{0}/scripts/myproject.wsgi .'.format(
-            fab_settings.PROJECT_NAME))
+            PROJECT_NAME))
         sed('myproject.wsgi', 'ENV_USER', fab_settings.ENV_USER)
         sed('myproject.wsgi', 'VENV_NAME', fab_settings.VENV_NAME)
         sed('myproject.wsgi', 'DJANGO_APP_NAME', fab_settings.DJANGO_APP_NAME)

0e4648d51ff76440455ba393dea8d2cf91cec001
Martin Brochhaus <mbrochh@gmail.com>
2012-03-20 01:03:28 +0800

    Added # NOQA flags

--- website/webapps/django/project/settings/installed_apps/cmsplugin_blog.py
+++ website/webapps/django/project/settings/installed_apps/cmsplugin_blog.py
@@ -1,7 +1,7 @@
 """Settings for the ``cmsplugin_blog`` app."""
 
 JQUERY_JS = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js'
-JQUERY_UI_JS = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js'
-JQUERY_UI_CSS = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/smoothness/jquery-ui.css'
+JQUERY_UI_JS = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js'  # NOQA
+JQUERY_UI_CSS = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/smoothness/jquery-ui.css'  # NOQA
 CMSPLUGIN_BLOG_PLACEHOLDERS = ('text',)
 BLOG_PAGINATE_BY = 8
